b5909d140dfc0d2a1add56177b5601aa49d260ea
Gowtham-Bioinfo <dileepgowtham1947@gmail.com>
2026-05-14 19:32:57 +0530

    Add exercises for set operations in set.py

---
+++ set.py
@@ -0,0 +1,10 @@
+
+
+Exercises: Level 1
+
+
+Find the length of the set it_companies
+Add 'Twitter' to it_companies
+Insert multiple IT companies at once to the set it_companies
+Remove one of the companies from the set it_companies
+What is the difference between remove and discard
