007e5a4f33c416988c76e4a47a70ffab52944286
Gowtham-Bioinfo <dileepgowtham1947@gmail.com>
2026-05-14 12:26:58 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -15,3 +15,42 @@ Modify the siblings tuple and add the name of your father and mother and assign 
 ## MY CODE FOR EXERCISE 1 PROBLEMS
 
 
+tple = tuple()
+
+sisters = ("Deepika", "Sonu", "Laddu", "Elina", "Rosy", "Mary")
+brothers = ("Nihaan", "Avyukt", "Pani", "Gowtham", "Sohan")
+
+siblings = sisters + brothers
+print(f"Siblings : ", siblings)
+print(len(siblings))
+
+family_mem = siblings + ("Ramesh", "Varalakshmi")
+print(family_mem)
+
+
+
+
+## Exercises: Level 2
+## Day 5  : 30 Days of python programming
+
+
+
+Unpack siblings and parents from family_members
+Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+Change the about food_stuff_tp tuple to a food_stuff_lt list
+Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+Slice out the first three items and the last three items from food_stuff_lt list
+Delete the food_stuff_tp tuple completely
+Check if an item exists in tuple:
+Check if 'Estonia' is a nordic country
+
+Check if 'Iceland' is a nordic country
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+
+
+## MY CODE FOR EXERCISE 2 PROBLEMS
+
+
+
