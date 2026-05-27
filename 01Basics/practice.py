179b0150d27038e1bf567b984b877cbf811bb00b
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-05-27 11:18:17 +0530

    Practice

---
+++ 01Basics/practice.py
@@ -0,0 +1,43 @@
+# Operators Practise :
+
+
+age = int(20)
+height = float(5.8) 
+comp = 18j
+
+
+base = int(input("Enter Base :"))
+height = int(input("Enter Height :"))
+
+area = 0.5*base*height
+
+print(f"The Area of Triangle is {area}")
+
+
+side_a = int(input("Enter Side A :"))
+side_b = int(input("Enter Side B :"))
+side_c = int(input("Enter Side C :"))
+
+perimeter = side_a + side_b + side_c
+
+print(perimeter)
+
+
+length = int(input("Enter Length :"))
+width = int(input("Enter Width :"))
+
+area = length * width
+perimeter = 2 *(length+width)
+
+print(area)
+print(perimeter)
+
+
+radius = float(input("Enter Radius of Circle :"))
+pi = 3.14
+
+area = pi*radius*radius
+circum = 2*pi*radius
+
+print(f"Area Of Circle is {area}")
+print(f"Circumference Of Circle is {circum}")
