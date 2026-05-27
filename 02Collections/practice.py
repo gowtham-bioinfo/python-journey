53d5ffc8415dfb6f7615ab9fe4f24bb97dea281a
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-05-27 12:17:18 +0530

    Practised Set

---
+++ 02Collections/practice.py
@@ -0,0 +1,63 @@
+# Set Practice 
+
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+
+print(len(it_companies))
+
+it_companies.add("Twitter")
+print(it_companies)
+it_companies.update("AWS", "JP Morgan", "Deliotte", "Veetech")
+print(it_companies)
+
+print(it_companies)
+
+
+a = "Dileep"
+b = "Gowtham"
+
+jn = a + b
+print(jn)
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+st = A.intersection(B)
+print(st)
+
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+st = B.issubset(B)
+print(st)
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+st = A.isdisjoint(B)
+print(st)
+
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+
+jn = a+b
+b = jn
+print(b)
+
+
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+st = {19, 22, 24, 25, 26}
+lst = len(age)
+st = len(st)
+
+
+
+
+if lst > st :
+    print("List Is Bigger")
+elif st > lst :
+    print("Set Is Biiger")
+else :
+    print("Both are Equal")
