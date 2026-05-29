29a0cbd622a8ecf8c13bccac092aece81a73bfba
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-05-29 11:06:26 +0530

    Modle

--- 05Modules/exercises.py
+++ 05Modules/exercises.py
@@ -4,3 +4,11 @@ import string
 def random_user_id():
     characters = string.ascii_lowercase + string.digits
     return ''.join(random.choice(characters) for _ in range(6))
+
+
+def generate_name(first_name, last_name) :
+    space = " "
+    full_name = first_name + space + last_name
+    return full_name
+
+print(generate_name("Dileep","Gowtham"))
--- 05Modules/practice.py
+++ 05Modules/practice.py
@@ -1,3 +1,8 @@
 import exercises
 
 print(exercises.random_user_id())
+
+
+import exercises
+
+print(exercises.generate_name("Dileep", "Gowtham"))
