eaedb53c5bfaff41fb3d2a5b314bc5e0cbfecfac
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-05-29 12:54:00 +0530

    Modules

--- 05Modules/practice.py
+++ 05Modules/practice.py
@@ -12,3 +12,20 @@ import string
 def random_user () :
     characters = string.ascii_lowercase + string.digits
     return "".join(random.choice(characters) for _ in range(6))
+
+
+import random
+import string
+
+def user_id_gen_by_user () :
+    char = int(input("Enter Characters :"))
+    count = int(input("Enter Numbers Of ID's :"))
+
+    characters = string.ascii_letters + string.digits
+
+    for _ in range (count) :
+        print("".join(random.choice(characters) for _ in range (char)))
+        
+
+
+user_id_gen_by_user()
