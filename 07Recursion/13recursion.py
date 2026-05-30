464e5e12cc966a53c7b3a9012d6d05b7346ba74e
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-05-30 22:10:48 +0530

    recursion

---
+++ 07Recursion/13recursion.py
@@ -0,0 +1,23 @@
+# Day 13 : 30 Days of python programming 
+
+# Exercise 1 – Basics
+# Write a recursive function to calculate the factorial of a number.
+# Write a recursive function to find the sum of the first n natural numbers.
+# Write a recursive function to print numbers from n to 1.
+# Write a recursive function to print numbers from 1 to n.
+# Write a recursive function to calculate the power of a number (x^n).
+# Write a recursive function to find the sum of digits of a number.
+# Write a recursive function to count the number of digits in a number.
+# Write a recursive function to reverse a string.
+# Write a recursive function to check whether a string is a palindrome.
+# Write a recursive function to find the product of two numbers using repeated addition.
+
+
+
+
+
+## MY CODE FOR ALL EX1 PROBLEMS
+
+
+#1
+
