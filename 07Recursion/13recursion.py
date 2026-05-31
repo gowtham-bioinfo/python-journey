c8fd3f3cc65884521bfddcc043f63ef9383c696f
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-05-31 13:46:41 +0530

    Recursion

--- 07Recursion/13recursion.py
+++ 07Recursion/13recursion.py
@@ -21,3 +21,12 @@
 
 #1
 
+def factorial(n):
+    if n == 0 or n == 1:  
+        return 1
+    else:
+        return n * factorial(n - 1) 
+
+
+num = 5
+print("Factorial of", num, "is", factorial(num))
