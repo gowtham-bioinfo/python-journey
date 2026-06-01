6e2a79f550521ed8d5e88de6f7af7fbd8591d714
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-01 16:33:42 +0530

    Recursion

--- 07Recursion/practice.py
+++ 07Recursion/practice.py
@@ -12,3 +12,37 @@ def calc_sum (n) :
 sum = calc_sum(5)
 
 print(sum)
+
+
+def print_list(list, idx=0) :
+    if (idx == len(list)) :
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+fruits = ["Mango", "Banana", "Apple", "Orange"]
+
+print(fruits)
+
+
+
+def calc_sum (n) :
+    if (n == 0 ) :
+        return 0
+    return calc_sum(n-1) + n
+sum = calc_sum(5)
+print(sum)
+
+
+
+def print_list(list, idx = 0) :
+    if (idx == len(list)) :
+        return 
+    print(list[idx])
+    print_list(list, idx+1)
+
+fruits = ["Mango", "Banana", "Apple", "Orange"]
+
+print_list(fruits)
+
+
