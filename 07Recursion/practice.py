055699ac06923cc34f28fd888f6c3a4450c3f357
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-01 22:57:07 +0530

    Recursion

--- 07Recursion/practice.py
+++ 07Recursion/practice.py
@@ -65,3 +65,12 @@ def print_numbers (n) :
     print_numbers(n-1) 
 
 print_numbers(5)
+
+
+def print_numbers (n) :
+    if n == 0  :
+        return
+    print_numbers(n-1)
+    print(n)
+
+print_numbers(5)
