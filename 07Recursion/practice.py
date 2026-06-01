f1c43a7a02bebfefee8dfd9aa5c2ae33896fd1f1
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-01 20:27:21 +0530

    Practicr

--- 07Recursion/practice.py
+++ 07Recursion/practice.py
@@ -48,3 +48,11 @@ def factorial(n) :
     return factorial(n-1) * n
 
 print(factorial(4))
+
+
+
+def sum_of_n(n) :
+    if n == 0 :
+        return 0
+    return sum_of_n(n-1) + n
+print(sum_of_n(4))
