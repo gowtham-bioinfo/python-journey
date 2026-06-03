b470faf4aca43160e1b2abaedbb425e97f6a0de7
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-03 22:24:13 +0530

    recursion

--- 07Recursion/13recursion.py
+++ 07Recursion/13recursion.py
@@ -219,3 +219,14 @@ def fibonacci_series(n, series=None):
 
 
 print(fibonacci_series(6)) 
+
+
+#3
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+print(gcd(48, 18))  
