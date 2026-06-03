9c07b13326a13fbde7d130eafc8989a94b103dc3
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-03 22:14:52 +0530

    Recursion

--- 07Recursion/13recursion.py
+++ 07Recursion/13recursion.py
@@ -183,3 +183,14 @@ print("Product =", multiply(num1, num2))
 
 #1
 
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input"
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(6))  
