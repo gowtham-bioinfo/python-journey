d962a870bc3e835e59549f33f57e742b44ed0f1d
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-03 22:20:20 +0530

    Update practice.py

--- 07Recursion/practice.py
+++ 07Recursion/practice.py
@@ -144,3 +144,19 @@ num1 = 5
 num2 = 4
 print("Product =", multiply(num1, num2))
 
+
+
+
+
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input"
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(6))  
+
