f99c39986cc66743203ec26a677db16f5be2ed2f
Dileep Gowtham <dileepgowtham1947@gmail.com>
2026-06-04 16:14:18 +0530

    Practice

--- 07Recursion/practice.py
+++ 07Recursion/practice.py
@@ -191,3 +191,11 @@ def power (x, n) :
     return x * power(x, n-1)
 
 print(power(4,5))
+
+
+def sum_of_digits(n) :
+    if n == 0 :
+        return 0
+    return (n % 10) + sum_of_digits(n // 10)
+
+print(sum_of_digits(8))
